2f06450b53cc2d07bd8ed0d9ca41d457cf41e570
Harish <114534778+harishsg993010@users.noreply.github.com>
2022-12-31 00:43:15 +0530

    Update linux_kill_shadow_passwd.py

--- linux_kill_shadow_passwd.py
+++ linux_kill_shadow_passwd.py
@@ -2,18 +2,28 @@ import os
 import psutil
 import time
 
+# Set the names of the processes to whitelist
+whitelist = [
+    "login",
+    "sshd",
+    "systemd",
+    "gnome-terminal",
+]
+
 while True:
     # Get a list of all running processes
     processes = psutil.process_iter()
     for process in processes:
         try:
-            # Get the list of open files for the process
-            open_files = process.open_files()
-            for file in open_files:
-                # Check if the process is accessing the passwd or shadow file
-                if file.path == "/etc/passwd" or file.path == "/etc/shadow":
-                    # Kill the process
-                    process.kill()
+            # Check if the process is not on the whitelist
+            if process.name() not in whitelist:
+                # Get the list of open files for the process
+                open_files = process.open_files()
+                for file in open_files:
+                    # Check if the process is accessing the passwd or shadow file
+                    if file.path == "/etc/passwd" or file.path == "/etc/shadow":
+                        # Kill the process
+                        process.kill()
         except Exception:
             pass
     # Sleep for a short period of time before checking for more processes
